87f6a8a632bfd649fd166a41c751cc1dca17113c
Lucas Fernandes <lucasbfnascimento@gmail.com>
2019-12-09 19:48:15 -0300

    Versão final do Projeto

--- src/main/java/br/com/pontorh/util/DAOFactory.java
+++ src/main/java/br/com/pontorh/util/DAOFactory.java
@@ -1,8 +1,15 @@
 package br.com.pontorh.util;
 
 import br.com.pontorh.registro.*;
+import br.com.pontorh.usuario.*;
 
 public class DAOFactory {
+	
+	public static UsuarioDAO criarUsuarioDAO() { //Médodo que instanciará um objeto da Classe UsuarioDAOHibernate e realizará a atribuição da Session ao objeto instanciado.
+		UsuarioDAOHibernate usuarioDAO = new UsuarioDAOHibernate();
+		usuarioDAO.setSession(HibernateUtil.getSessionFactory().getCurrentSession());
+		return usuarioDAO;
+	}
 	
 	public static RegistroDAO criarRegistroDAO() { //Médodo que instanciará um objeto da Classe RegistroDAOHibernate e realizará a atribuição da Session ao objeto instanciado.
 		RegistroDAOHibernate registroDAO = new RegistroDAOHibernate();
